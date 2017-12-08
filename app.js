e1eab828f3edded7cf56d470004753bfbf30f2b5
andriiglukhyi <andrigluxii@gmail.com>
2017-12-08 13:56:21 -0800

    solv problem2

--- app.js
+++ app.js
@@ -33,11 +33,14 @@ Test this function by hand in the console to get it working, and when you think 
 
 // TODO: Write your code here
 function multiply(a,b){ //eslint-disable-line
-
+  var mult = a*b;
+  var arr = ' The product of ' + a + ' and ' + b + ' is ' + mult ;
+  var arr1 = [mult , arr];
+  return [arr1]
 }
 
 // TODO: Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+multiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
