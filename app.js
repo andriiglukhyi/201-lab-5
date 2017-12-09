6fe5574da55f2e8fe08b5638021050de4d9959de
andriiglukhyi <andrigluxii@gmail.com>
2017-12-08 16:36:07 -0800

    number4

--- app.js
+++ app.js
@@ -19,25 +19,6 @@ function multiply(a,b){ //eslint-disable-line
 }
 //
 
-// multiply(5,9);
-
-/* Problem 3
-Write a function called sumAndMultiply() that takes in three numbers as separate arguments and returns an array where the first element is the sum of those
-three numbers,
-
-the second element is the product of those three numbers,  and the third and fourth elements are strings that EXACTLY follow this example
- and concatenates messages using the arguments that were passed into the function:
-
-Third element: "4 and 7 and 5 sum to 16."
-Fourth element: "The product of 4 and 7 and 5 is 140."
-
-IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this function.
-To do addition, use your sum() function, and to do multiplication, use your multiply() function that you've already created.
-You're going to have to be resourceful to figure out how to do this.
-
-Test this function by hand in the console to get it working, and when you think it is finished, uncomment
- the call for the testSumAndMultiply() function and see if the test passes.*/
-
 // TODO: Write your code here
 
 function sumAndMultiply(a,b,c){ //eslint-disable-line
@@ -46,32 +27,27 @@ function sumAndMultiply(a,b,c){ //eslint-disable-line
   var prod = multiply(a,b);
   var bigprod = multiply(prod[0], c)
   var str1 = a + ' and ' + b + ' and ' + c + ' sum to ' + bigsum[0];
-  var str2 = 'The product of ' + a + ' and ' +  b + ' and ' +  c + ' is ' + bigprod[0] ;
+  var str2 = 'The product of ' + a + ' and ' + b + ' and ' +  c + ' is ' + bigprod[0] ;
   return [bigsum[0], bigprod[0], str1, str2]
+
+}
 
 }
 
 // TODO: Here is the test for sumAndMultiply(); uncomment it to run it
 sumAndMultiply(4,7,5);
-
-// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
-
-/////////////////////////////////////
-/* Problem 4
-Write a function called sumArray() that takes in an array of numbers as its single argument and then returns an array where the first element is the sum of the numbers in the array, and the second element is a string that EXACTLY follows this example and concatenates a message using the arguments that were passed into the function:
-
-"2,3,4 was passed in as an array of numbers, and 9 is their sum."
-
-IMPORTANT DETAIL: You may not use the arithmetic operator + in this function. To do addition, use your sum() function that you've already created. You're going to have to be resourceful to figure out how to do this.
-
-Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumArray() function and see if the test passes.*/
-
 // TODO: Write your code here
 var testArray = [2,3,4]; //eslint-disable-line
 
-function sumArray(testArray){ //eslint-disable-line
 
+function sumArray(testArray1){ //eslint-disable-line
+  var num1 = sumAndMultiply(testArray1[0], testArray1[1], testArray1[2]);
+  var num1c = num1[0];
+  var stri1 = testArray1[0] + ' , ' + testArray1[1] + ' , ' + testArray1[2] + ' was passed in as an array of numbers, and ' + num1c + ' is their sum.';
+  return [num1c, stri1]
 }
+
+sumArray(testArray);
 
 // TODO: Here is the test for sumArray(); uncomment it to run it
 // testSumArray(testArray);
